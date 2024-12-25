2da7b46ac5aeffbd15d3a675fe042ce7052f1031
NanaKwateng <nanakwateng172@gmail.com>
2024-12-25 04:18:56 +0000

    first commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -25,12 +25,12 @@ const Nav = () => {
 
     const pathname = usePathname()
   return (
-    <nav className="flex gap-8 px-9 py-3 rounded-3xl bg-[#232329]">
+    <nav className="flex gap-8 px-9 py-3 rounded-3xl bg-white/100 shadow-sm dark:bg-[#232329]">
         {links.map((links, i) => {
-            return <Link href={links.path} key={i} className={`${links.path === pathname && "text-slate-500 border-b-2 border-slate-500"} capitalize hover:text-slate-300 transition-colors`}>{links.name}</Link>
+            return <Link href={links.path} key={i} className={`${links.path === pathname && "text-accent border-b-2 border-accent"} capitalize hover:text-slate-300 transition-colors`}>{links.name}</Link>
         })}
     </nav>
   )
 }
-
+//bg-[#232329]
 export default Nav
